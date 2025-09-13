57f3da72a1e86476f049019116a36a4270cda331
Madu <135995685+Mariaecota@users.noreply.github.com>
2025-09-13 19:14:11 -0300

    Decodificador de senhas e ci

---
+++ codificador.py
@@ -0,0 +1,31 @@
+def codificar(senha):
+    # codifica invertendo a string + um deslocamento simples de caractere
+    return "".join([chr(ord(c)+2) for c in senha[::-1]])
+
+def decodificar(token):
+    # decodifica revertendo o processo
+    return "".join([chr(ord(c)-2) for c in token])[::-1]
+
+def main():
+    while True:
+        print("\n1 - Codificar senha")
+        print("2 - Decodificar senha")
+        print("3 - Sair")
+        opc = input("Escolha uma opção: ")
+        
+        if opc == "1":
+            senha = input("Digite a senha: ")
+            token = codificar(senha)
+            print("Senha codificada:", token)
+        elif opc == "2":
+            token = input("Digite o token: ")
+            senha = decodificar(token)
+            print("Senha decodificada:", senha)
+        elif opc == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida!")
+
+if __name__ == "__main__":
+    main()
