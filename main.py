f2b601b4f34bbf1290cca7ecf5c41487972600e4
Madu <135995685+Mariaecota@users.noreply.github.com>
2025-09-13 17:25:03 -0300

    nova alteração do fast

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # fast
+
+#def somar(a, b):
+#    return a + b
+
+#if __name__ == "__main__":
+#    x = 10
+#    y = 5
+#    resultado = somar(x, y)
+#    print(f"A soma de {x} + {y} é = {resultado}")
