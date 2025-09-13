88653042ca656717c549490ae7739782b34c631d
Madu <135995685+Mariaecota@users.noreply.github.com>
2025-09-13 18:16:44 -0300

    v.03

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # fast
 
 #<<<<<<< alteração-endpoint
-#def somar(a, b):
-#    return a + b
+def somar(a, b):
+    return a + b
 
-#if __name__ == "__main__":
-#    x = 10
-#    y = 5
-#    resultado = somar(x, y)
-#    print(f"A soma de {x} + {y} é = {resultado}")
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    resultado = somar(x, y)
+    print(f"A soma de {x} + {y} é = {resultado}")
 #=======
 #hellow wordffgfg
 #>>>>>>> main
